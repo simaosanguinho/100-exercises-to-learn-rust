d3aac35700f4acf2810cc994d519e01a54e83a9b
Simão Sanguinho <simao.sanguinho@tecnico.ulisboa.pt>
2025-10-29 23:12:26 +0000

    feat: add traits 00-04 solutions

--- exercises/04_traits/02_orphan_rule/src/lib.rs
+++ exercises/04_traits/02_orphan_rule/src/lib.rs
@@ -4,8 +4,13 @@
 //  Look at the compiler error to get familiar with what it looks like.
 //  Then delete the code below and move on to the next exercise.
 
-impl PartialEq for u32 {
+
+// to solve this we can have our own version of teh u32
+struct MyU32(u32);
+
+
+impl PartialEq for MyU32 {
     fn eq(&self, _other: &Self) -> bool {
-        todo!()
+        self == _other
     }
 }
