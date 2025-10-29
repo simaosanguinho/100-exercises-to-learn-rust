fee8b970e9ff73a7fb8c7968430fb3eb95184ac6
Simão Sanguinho <simao.sanguinho@tecnico.ulisboa.pt>
2025-10-29 16:17:57 +0000

    feat: add ticket 10-12 solutions

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -11,3 +11,78 @@
 // Integration here has a very specific meaning: they test **the public API** of your project.
 // You'll need to pay attention to the visibility of your types and methods; integration
 // tests can't access private or `pub(crate)` items.
+
+pub struct Order {
+    product_name: String,
+    quantity: i32,
+    unit_price: i32,
+
+}
+
+
+impl Order {
+    pub fn new(product_name: String, quantity: i32, unit_price: i32) -> Order {
+        Self::validate_product_name(&product_name);
+        Self::validate_quantity(&quantity);
+        Self::validate_unit_price(&unit_price);
+        Order {
+            product_name,
+            quantity,
+            unit_price,
+        }
+    }
+
+    fn validate_product_name(name: &str) {
+        if name.is_empty() {
+            panic!("Product name cannot be empty");
+        }
+
+        if name.len() > 300 {
+            panic!("Product name cannot be longer than 300 bytes")
+        }
+    }
+
+    fn validate_quantity(quantity: &i32) {
+        if quantity <= &0 {
+            panic!("Quantity cannot be less or equal to zero")
+        }
+    }
+
+    fn validate_unit_price(unit_price: &i32) {
+        if unit_price <= &0 {
+            panic!("Unit price cannot be less or equal to zero")
+        }
+    }
+
+
+    pub fn product_name(&self) -> &String {
+        &self.product_name
+    }
+
+    pub fn quantity(&self) -> &i32 {
+        &self.quantity
+    }
+
+    pub fn unit_price(&self) -> &i32 {
+        &self.unit_price
+    }
+
+    pub fn set_product_name(&mut self, new_name: String) {
+        Self::validate_product_name(&new_name);
+        self.product_name = new_name;
+    }
+
+    pub fn set_quantity(&mut self, new_quantity: i32) {
+        Self::validate_quantity(&new_quantity);
+        self.quantity = new_quantity;
+    }
+
+    pub fn set_unit_price(&mut self, new_price: i32) {
+        Self::validate_unit_price(&new_price);
+        self.unit_price = new_price;
+    }
+
+    pub fn total(&self) -> i32 {
+        self.quantity * self.unit_price
+    }
+}
